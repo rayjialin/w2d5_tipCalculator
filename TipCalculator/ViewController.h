b0c72c6bdcf55c2fdc4f3a6fd82d1c20b2373c81
rayjialin <ruijialin@gmail.com>
2018-04-20 11:08:02 -0700

    initial commit for w2d5-TipCalculator

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by ruijia lin on 4/20/18.
+//  Copyright © 2018 ruijia lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
